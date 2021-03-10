a5ebf1b98b1d2543a8235486918f78f9359675db
Laio <rao418065@gmail.com>
2021-03-10 17:08:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
 const app = express();
 const port = 3000;
 
@@ -18,8 +19,13 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
+// 樣板引擎相關設定(先命名為hbs, 之後是exphbs的相關參數設定)
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// 啟用樣板引擎(也可以只寫啟用的部分，推測沒寫設定應該會有預設值或是就沒有設定了(?))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-    res.send('Hello World! ')
+    res.render('index')
 })
 
 app.listen(port, () => {
