9b7de82dd3af715b2697981fb0ec8d6d29fd1af6
Laio <rao418065@gmail.com>
2021-03-16 14:56:05 +0800

    feat: modify detail page ui

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ const app = express();
 const port = 3000;
 
 require('./config/mongoose')
-// mongoose.connect('mongodb://localhost/todo-list' , {useNewUrlParser: true, useUnifiedTopology: true}) // 設定連線到 mongoDB
-
-// // 取得資料庫連線狀態
-// const db = mongoose.connection;
-
-// // 連線異常
-// db.on('error', () => {
-//     console.log('mongodb error!')
-// })
-
-// // 連線成功
-// db.once('open', () => {
-//     console.log('mongodb connected!')
-// })
 
 // 樣板引擎相關設定(先命名為hbs, 之後是exphbs的相關參數設定)
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
